1ecf2f81003ef0720265ba17fd73ed0a0c84ede0
Konyee11 <konyee.527@gmail.com>
2024-11-28 10:39:14 +0900

    ファイル名変更

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./styles.css";
 
-export default function App() {
+export const App = () => {
     return (
         <div className="App">
             <h1>Hello World!</h1>
             <h2>こんにちは！</h2>
         </div>
     );
-}
+};
