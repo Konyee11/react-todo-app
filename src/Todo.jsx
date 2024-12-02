672e5656582bf5f948c0d136af2862419034bdd2
Konyee11 <konyee.527@gmail.com>
2024-12-02 21:26:48 +0900

    styles.cssの相対パスを修正

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../styles/styles.css";
+import "./styles/styles.css";
 import { InputTodo } from "./components/InputTodo";
 import { IncompleteTodo } from "./components/IncompleteTodo";
 import { CompleteTodo } from "./components/CompleteTodo";
